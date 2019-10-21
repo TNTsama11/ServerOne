f09d44bdb7c4887d6913b51075c80efedf41411b
TNTsama11 <32362871+TNTsama11@users.noreply.github.com>
2019-10-21 17:24:59 +0800

    开始开发匹配功能

---
+++ CommunicationProtocol/Code/MatchCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommunicationProtocol.Code
+{
+    public class MatchCode
+    {
+        public const int MATCH_ENTER_CREQ = 0;
+
+        public const int MATCH_ENTER_SREP = 1;
+
+        public const int MATCH_EXIT_CREQ = 2;
+
+        public const int MATCH_EXIT_SREP = 3;
+
+        public const int MATCH_START = 4;
+    }
+}
